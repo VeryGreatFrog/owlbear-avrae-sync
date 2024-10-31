9fd211c54b599acfff15728dbd80362d33613e2f
VeryGreatFrog <bvl46z@gmail.com>
2024-10-31 12:17:14 +0100

    Update main.ts

--- extension/src/main.ts
+++ extension/src/main.ts
@@ -1,16 +1,26 @@
 import './style.css'
 import OBR from '@owlbear-rodeo/sdk'
-
+import { Shape } from '@owlbear-rodeo/sdk'
+import { getPluginId } from './helper.ts'
+import { isPlainObject } from './helper.ts'
 OBR.onReady(() => {
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1> Avrae Owlbear Sync </h1>
     <div> Select a discord channel to link to: 1301245033302851614 </div>
     <input type="text" placeholder="Channel ID" id="channel-id" value="1301245033302851614"/>
-    <div id="table"> </div>
+    <button type="button" id="clear-tokens"> Clear all effects </button>
   </div>
 `
 })
 
 
+document.querySelector("#clear-tokens")?.addEventListener("click", async () => {
+  const currentAttachments = await OBR.scene.items.getItems<Shape>((item) => {
+    const metadata = item.metadata[getPluginId("metadata")];
+    return Boolean(isPlainObject(metadata));
+});
+await OBR.scene.items.deleteItems(currentAttachments.map((a) => a.id))
+
+})
 import "./socket.ts"
